2f6b0d6de9612f17cfc9e8585b12cab59e3dc8b0
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-06-04 16:04:44 -0400

    images loaded in Ion Content

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,35 +2,63 @@ import {
   IonContent,
   IonHeader,
   IonPage,
-  IonTitle,
   IonToolbar,
   IonButtons,
   IonMenuButton,
   IonButton,
-  IonIcon,
-  IonItemSliding,
   IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonRippleEffect,
 } from "@ionic/react";
+
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 import logo from "../images/sworkit-logo.svg";
-// import menu_outline from "../images/menu-outline.svg";
+import strength from "../images/category-strength.jpeg";
+import cardio from "../images/category-cardio.jpeg";
+import yoga from "../images/category-yoga.jpeg";
+import stretching from "../images/category-stretching.jpeg";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonButton>
-                <IonMenuButton autoHide={false} style={{ color: "black" }}/>
-              </IonButton>
-            </IonButtons>
-            <IonImg src={logo} style={{ width: "200px" }} />
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton>
+              <IonMenuButton autoHide={false} style={{ color: "black" }} />
+            </IonButton>
+          </IonButtons>
+          <IonImg src={logo} style={{ width: "200px" }} />
+        </IonToolbar>
+      </IonHeader>
+      <ExploreContainer name="Essentials" />
+      <IonContent fullscreen className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>1 of 3</IonCol>
+          </IonRow>
+          <h4>Essentials</h4>
+          <IonRow>
+            <IonCol size-lg="3">
+              <IonImg src={strength} />
+            </IonCol>
+
+            <IonCol size-lg="3">
+              <IonImg src={cardio} />
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonImg src={yoga} />
+            </IonCol>
+            <IonCol>
+              <IonImg src={stretching} />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
