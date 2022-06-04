7f837ae1916657db3354c33155a503a1a94a0d43
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-06-04 11:15:04 -0400

    footer configured still working on toolbar/header

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,14 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonButton,
+  IonIcon,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import logo from "../images/sworkit-logo.svg";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
+        <IonButtons slot="start">
+          <IonMenuButton autoHide={false} />
+        </IonButtons>
+        <IonButtons slot="secondary">
+          <IonButton>
+            <IonIcon slot="icon-only" icon={logo} />
+          </IonButton>
+        </IonButtons>
+        <IonTitle>Left side menu toggle</IonTitle>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
